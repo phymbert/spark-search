bf6bf1a52e6adaa8994397011d985a2bf8fe0079
Pierrick HYMBERT <pierrick.hymbert@gmail.com>
2020-07-05 23:10:23 +0200

    Fix CI YARN / Standalone file copy

--- examples/src/main/scala/org/apache/spark/search/rdd/ExampleData.scala
+++ examples/src/main/scala/org/apache/spark/search/rdd/ExampleData.scala
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -15,9 +15,16 @@
  */
 package org.apache.spark.search.rdd
 
-import org.apache.spark.SparkFiles
+import java.io.File
+import java.net.URL
+
+import org.apache.hadoop.conf.Configuration
+import org.apache.hadoop.fs.{FileSystem, Path}
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.SparkSession
+
+import scala.language.postfixOps
+import scala.sys.process._
 
 object ExampleData {
 
@@ -28,15 +35,28 @@ object ExampleData {
   def loadComputerReviews(spark: SparkSession): (RDD[Review], RDD[Review]) = {
     import spark.implicits._
 
+    val hadoopConf = new Configuration()
+    val hdfs = FileSystem.get(hadoopConf)
+
     // Amazon computers reviews
     println("Downloading amazon computers reviews...")
-    spark.sparkContext.addFile("http://snap.stanford.edu/data/amazon/productGraph/categoryFiles/reviews_Computers.json.gz")
-    val computersReviewsRDD = spark.read.json(SparkFiles.get("reviews_Computers.json.gz")).as[Review].rdd.repartition(4)
+    val computersReviewFile = File.createTempFile("reviews_Computers", ".json.gz")
+    computersReviewFile.deleteOnExit()
+    new URL("http://snap.stanford.edu/data/amazon/productGraph/categoryFiles/reviews_Computers.json.gz") #> computersReviewFile !!
+
+    hdfs.copyFromLocalFile(new Path(computersReviewFile.getAbsolutePath), new Path("/reviews_Computers.json.gz"))
+
+    val computersReviewsRDD = spark.read.json("/reviews_Computers.json.gz").as[Review].rdd.repartition(4)
 
     // Amazon software reviews
     println("Downloading amazon software reviews...")
-    spark.sparkContext.addFile("http://snap.stanford.edu/data/amazon/productGraph/categoryFiles/reviews_Software_10.json.gz")
-    val softwareReviewsRDD = spark.read.json(SparkFiles.get("reviews_Software_10.json.gz")).as[Review].rdd.repartition(4)
+    val softwareReviewsFile = File.createTempFile("reviews_Software", ".json.gz")
+    softwareReviewsFile.deleteOnExit()
+    new URL("http://snap.stanford.edu/data/amazon/productGraph/categoryFiles/reviews_Software_10.json.gz") #> softwareReviewsFile !!
+
+    hdfs.copyFromLocalFile(new Path(softwareReviewsFile.getAbsolutePath), new Path("/reviews_Software.json.gz"))
+
+    val softwareReviewsRDD = spark.read.json("/reviews_Software.json.gz").as[Review].rdd.repartition(4)
 
     (computersReviewsRDD, softwareReviewsRDD)
   }
