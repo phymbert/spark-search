31cd3087c066ba3947185df9b6b1dabe25e9fc49
Pierrick HYMBERT <pierrick.hymbert@gmail.com>
2021-09-20 23:48:04 +0200

    Fix sql compilation Change default 2.11 spark/hadoop version

--- sql/src/main/scala/org/apache/spark/search/sql/physical.scala
+++ sql/src/main/scala/org/apache/spark/search/sql/physical.scala
@@ -50,7 +50,7 @@ case class SearchJoinExec(left: SparkPlan, right: SparkPlan, searchExpression: E
       }
       case _ => throw new IllegalArgumentException
     }
-
+é
     searchRDD.matchesQuery(leftRDD.zipWithIndex().map(_.swap), qb, 1)
       .filter(_.hits.nonEmpty) // TODO move this filter at partition level
       .map(m => toRow(m.doc.asInstanceOf[UnsafeRow], m.hits.head.score))
